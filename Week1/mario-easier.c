cca415270a9c689f46f31ff15482419053f9f6a5
Siddharth Nikhil <mailtosiddnikh@gmail.com>
2020-05-05 23:12:26 +0530

    Create mario-easier.c

---
+++ Week1/mario-easier.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void){
+    char c = '#';
+    int n = get_int("Enter the number of rows you want\n") + 1;
+    for(int i = 0; i < n; i++){
+
+        for(int f = 0; f < n; f++){
+
+            if(f < n - i){
+                printf(" ");
+            }
+            else{
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+}
