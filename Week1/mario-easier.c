93e94452a834a17531c355e81a69d03aedbadda5
Siddharth Nikhil <mailtosiddnikh@gmail.com>
2020-05-06 00:31:25 +0530

    Update mario-easier.c

--- Week1/mario-easier.c
+++ Week1/mario-easier.c
@@ -4,7 +4,7 @@
 int main(void){
     char c = '#';
     int n=-1; 
-    while(n<0){
+    while(n<0 && n>8){
         n = get_int("Enter the number of rows you want\n") + 1;
     }
     for(int i = 0; i < n; i++){
