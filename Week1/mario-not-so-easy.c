4b6a4d925a71c5a635b27493ac94c96f5dadf8fe
Siddharth Nikhil <mailtosiddnikh@gmail.com>
2020-05-05 23:32:42 +0530

    Create mario-not-so-easy.c

---
+++ Week1/mario-not-so-easy.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void){
+
+    int n = get_int("Enter the number of rows you want\n");
+    
+    for(int i = 1; i <= n; i++){
+    
+        for(int j = 0; j<=n+1+i; j++){
+        
+            if(j<=n-i || j == n+1){
+                printf(" ");
+            }
+            
+            else{
+                printf("#");
+            }
+            
+        }
+        printf("\n");
+    }
+}
