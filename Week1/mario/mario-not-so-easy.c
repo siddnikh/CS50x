ed615348ae361562d9fc072dc77c4520d6f3b0cd
Siddharth Nikhil <mailtosiddnikh@gmail.com>
2020-05-07 12:07:30 +0530

    Update mario-not-so-easy.c

--- Week1/mario/mario-not-so-easy.c
+++ Week1/mario/mario-not-so-easy.c
@@ -4,22 +4,22 @@
 int main(void){
 
     int n = -1;
-    while(n < 0 && n > 8){
-        get_int("Enter the number of rows you want\n");
+    while(n <=0 || n > 8){
+        n = get_int("Enter the number of rows you want\n");
     }
-    
+
     for(int i = 1; i <= n; i++){
-    
-        for(int j = 0; j<=n+2+i; j++){
-        
+
+        for(int j = 1; j<=n+2+i; j++){
+
             if(j<=n-i || j == n+1 || j == n+2){
                 printf(" ");
             }
-            
+
             else{
                 printf("#");
             }
-            
+
         }
         printf("\n");
     }
