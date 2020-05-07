5eefb9a540d15260b28a692540bd9f7ec939c8d3
Siddharth Nikhil <mailtosiddnikh@gmail.com>
2020-05-07 12:16:01 +0530

    Update mario-easier.c

--- Week1/mario/mario-easier.c
+++ Week1/mario/mario-easier.c
@@ -2,12 +2,13 @@
 #include<cs50.h>
 
 int main(void){
-    char c = '#';
-    int n=-1; 
-    while(n<0 && n>8){
-        n = get_int("Enter the number of rows you want\n") + 1;
+
+    int n=-1;
+
+    while(n <=0 || n>8){
+        n = get_int("Enter the number of rows you want\n") ;
     }
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i <= n; i++){
 
         for(int f = 0; f < n; f++){
 
